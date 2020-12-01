ec186e45bc8541b04aea478c225bd43c0931eb1d
Bem <59389836+Benmcn4123@users.noreply.github.com>
2020-12-02 08:02:13 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import sys, pygame
 from pygame.locals import *
 
+#import images
+
+player_anims = {"idle":[pygame.image.load("images/art1.png")], "run":[], "cannon_up_idle":[pygame.image.load("images/art4.png")], "cannon_up_run":[]}
+grass_tiles = {"default_grass":pygame.image.load("images/art8.png"), "top_grass":pygame.image.load("images/art9.png")}
+
+
 clock = pygame.time.Clock()
 pygame.init()
 
+WIN_SIZE = (800, 600)
+
 pygame.display.set_caption("game")
-win = pygame.display.set_mode((1500, 700), 0, 32)
+win = pygame.display.set_mode((WIN_SIZE), 0, 32)
+
+display = pygame.Surface((400, 300))
 
 moving_left = False
 moving_right = False
 
-player = pygame.Rect(250, 250, 20, 30)
+player = pygame.Rect(250, 0, 8, 15)
 
 # declare colours
 yellow = pygame.Color("#ffAB5E")
@@ -18,7 +28,8 @@ orange = pygame.Color("#ff7844")
 red = pygame.Color("#a64942")
 blue = pygame.Color("#23425f")
 
-font = pygame.font.SysFont(None, 30)
+font = pygame.font.SysFont(None, 15)
+bullet_image = pygame.image.load("images/art7.png")
 
 map = [
 ".................o....................",
@@ -38,10 +49,10 @@ map = [
 "######################################",
 ]
 
-SPEED = 5
+SPEED = 3
 y_velocity = 0
 x_velocity = 0
-BULLET_SPEED = 10
+BULLET_SPEED = 5
 
 
 cam_x = 250
@@ -49,7 +60,7 @@ coyote_timer = 0
 dir = "right"
 bullets = []
 looking_up = False
-tile_width = 40
+tile_width = 16
 
 class enemy:
     def __init__ (self, rect, type = "fodder"):
@@ -70,14 +81,14 @@ class enemy:
                         self.rect.top = tile.bottom
                     self.y_velocity = 0
 
-            self.y_velocity += 1
+            self.y_velocity += 0.5
             if self.y_velocity > 15:
                 self.y_velocity = 15
     def draw(self):
         rect = self.rect
-        pygame.draw.rect(win, red, (rect.x - cam_x, rect.y, rect.width, rect.height))
-        pygame.draw.rect(win, blue, (rect.x - cam_x + 5, rect.y + 10, 5, 5))
-        pygame.draw.rect(win, blue, (rect.right - cam_x - 10, rect.y + 10, 5, 5))
+        pygame.draw.rect(display, red, (rect.x - cam_x, rect.y, rect.width, rect.height))
+        pygame.draw.rect(display, blue, (rect.x - cam_x + 5, rect.y + 10, 5, 5))
+        pygame.draw.rect(display, blue, (rect.right - cam_x - 10, rect.y + 10, 5, 5))
 
 def on_ground(col_rect, col):
     for tile in col:
@@ -86,7 +97,7 @@ def on_ground(col_rect, col):
     return False
 
 def create_bullet():
-    bullet = {"size": 12, "x": player.left + 10, "y": player.top + 11, "x_velocity": 0, "y_velocity": 0}
+    bullet = {"size": 6, "x": player.left + 4, "y": player.top + 1, "x_velocity": 0, "y_velocity": 0}
     if looking_up:
         vel = y_velocity
         if vel > 0 :
@@ -106,26 +117,24 @@ for y in range(0, len(map)):
             new_enemy = enemy(pygame.Rect(x * tile_width, y * tile_width, 40, 20))
             enemies.append(new_enemy)
 
+tiles = []
+foreground = []
+
+for y in range(0, len(map)):
+    for x in range(0, len(map[y])):
+        if map[y][x] == "#":
+            rect = pygame.Rect(x * tile_width, y * tile_width, tile_width, tile_width)
+            tiles.append(rect)
+        elif map[y][x] == "v":
+            rect = pygame.Rect(x * tile_width, y * tile_width, tile_width, tile_width)
+            foreground.append(rect)
+
+
 while True:
     delta_time = clock.get_time() / 20
-    win.fill(yellow) # cls
+    display.fill(yellow) # cls
 
-    tiles = []
-    foreground = []
-
-
-
-    for y in range(0, len(map)):
-        for x in range(0, len(map[y])):
-            if map[y][x] == "#":
-                tile_width = 40
-                rect = pygame.Rect(x * tile_width, y * tile_width, tile_width, tile_width)
-                pygame.draw.rect(win, blue, (rect.x - cam_x, rect.y, rect.width, rect.height))
-                tiles.append(rect)
-            elif map[y][x] == "v":
-                foreground.append(((x * tile_width - cam_x, y * tile_width + tile_width), (x * tile_width - cam_x + tile_width - 1, y * tile_width + tile_width), (x * tile_width - cam_x + tile_width / 2, y * tile_width + tile_width / 2)))
-
-    test_rect = pygame.Rect(player.left, player.top, 20, 31)
+    test_rect = pygame.Rect(player.x, player.y, player.width, player.height + 1)
     grounded = on_ground(test_rect, tiles)
 
     if grounded:
@@ -148,7 +157,7 @@ while True:
                 create_bullet()
             if event.key == K_z and coyote_timer < 10:
                 coyote_timer = 10
-                y_velocity = -15
+                y_velocity = -8
             if event.key == K_w or event.key == K_UP:
                 looking_up = True
 
@@ -176,10 +185,7 @@ while True:
                 player.left = tile.right
             x_velocity = 0
 
-
-
-
-    cam_target = player.centerx - win.get_size()[0] / 2
+    cam_target = player.centerx - display.get_size()[0] / 2
     cam_x  = cam_target #+= (cam_target - cam_x) * 0.1
 
     player.top += y_velocity * delta_time
@@ -193,54 +199,58 @@ while True:
             y_velocity = 0
 
 
-    y_velocity += 1 * delta_time
+    y_velocity += 0.5 * delta_time
 
     if y_velocity > 15:
         y_velocity = 15
 
-    pygame.draw.rect(win, red, (player.x - cam_x, player.y, player.width, player.height))
-    if dir == "right" and not looking_up:
-        pygame.draw.rect(win, blue, (player.left + 7 - cam_x, player.top + 5, 22, 12))
-        pygame.draw.rect(win, blue, (player.left + 2 - cam_x, player.top + 2, 4, 4))
-    elif dir == "left" and not looking_up:
-        pygame.draw.rect(win, blue, (player.left - 9 - cam_x, player.top + 5, 22, 12))
-        pygame.draw.rect(win, blue, (player.right - 6 - cam_x, player.top + 2, 4, 4))
-    elif looking_up:
-        pygame.draw.rect(win, blue, (player.left + 2 - cam_x, player.top - 18, 14, 18))
-        pygame.draw.rect(win, blue, (player.right - 6 - cam_x, player.top + 2, 4, 4))
 
-    new_bullets = bullets.copy()
-    for bullet in bullets:
+
+    for bullet in bullets[:]:
         bullet["x"] += bullet["x_velocity"] * delta_time
         bullet["y"] += bullet["y_velocity"] * delta_time
         bullet_rect = pygame.Rect(bullet["x"], bullet["y"], bullet["size"], bullet["size"])
 
-        if bullet_rect.left < 0 + cam_x or bullet_rect.right > win.get_size()[0] + cam_x:
-            new_bullets.remove(bullet)
+        if bullet_rect.left < 0 + cam_x or bullet_rect.right > display.get_size()[0] + cam_x:
+            bullets.remove(bullet)
             break
 
         for tile in tiles:
             if bullet_rect.colliderect(tile):
-                new_bullets.remove(bullet)
+                bullets.remove(bullet)
                 break
 
 
-        pygame.draw.circle(win, red, (bullet_rect.x - cam_x, bullet_rect.y), bullet_rect.width/2, 0)
+        display.blit(bullet_image, (bullet_rect.x - 5 - cam_x, bullet_rect.y-5, bullet_rect.width + 10, bullet_rect.height + 10))
 
-    bullets = new_bullets.copy()
 
-    for enemy in enemies:
+    '''for enemy in enemies:
         enemy.update()
-        enemy.draw()
+        enemy.draw()'''
 
+    if dir=="right":
+        if looking_up:
+            display.blit(player_anims["cannon_up_idle"][0], (player.x - 4 - cam_x, player.y - 1, player.width, player.height))
+        else:
+            display.blit(player_anims["idle"][0], (player.x - 4 - cam_x, player.y - 1, player.width, player.height))
+    elif dir == "left":
+        if looking_up:
+            display.blit(pygame.transform.flip(player_anims["cannon_up_idle"][0], True, False), (player.x - 4 - cam_x, player.y - 1, player.width, player.height))
+        else:
+            display.blit(pygame.transform.flip(player_anims["idle"][0], True, False), (player.x - 4 - cam_x, player.y - 1, player.width, player.height))
+
+    for tile in tiles:
+        display.blit(grass_tiles["default_grass"], (tile.x - cam_x, tile.y, tile.width, tile.height))
     for foliage in foreground:
-        pygame.draw.polygon(win, blue, foliage)
+        display.blit(grass_tiles["top_grass"], (foliage.x - cam_x, foliage.y, foliage.width, foliage.height))
 
     text = font.render(str(clock.get_fps()), True, (0, 0, 0), (0, 0, 1))
     text.set_colorkey((0, 0, 1))
     text_rect = text.get_rect()
     text_rect
-    win.blit(text, text_rect)
+    display.blit(text, text_rect)
+
+    win.blit(pygame.transform.scale(display, WIN_SIZE), (0,0))
 
     pygame.display.update()
     clock.tick(60)
